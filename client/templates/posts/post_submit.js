3aa75a5376b0c8ece67dcf78294cf56b90e8221a
Vasil Vanchuk <v.vanchuk@tut.by>
2015-04-27 23:51:00 +0300

    Check rul unique

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,6 +11,10 @@ Template.postSubmit.events({
       // отобразить ошибку пользователю и прерваться
       if (error)
         return alert(error.reason);
+
+      if (result.postExists)
+        alert('This link has already been posted');
+      
       Router.go('postPage', {_id: result._id});
     });
   }
